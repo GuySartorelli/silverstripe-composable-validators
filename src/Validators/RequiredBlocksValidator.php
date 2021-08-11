0bf8458102af91aae98379d904a242903213f04d
Guy Sartorelli <guy.sartorelli@signify.co.nz>
2021-08-11 16:59:33 +1200

    refactor: Split validation of ElementalAreas and missing blocks into new methods.

--- src/Validators/RequiredBlocksValidator.php
+++ src/Validators/RequiredBlocksValidator.php
@@ -6,6 +6,7 @@ use DNADesign\Elemental\Forms\ElementalAreaField;
 use DNADesign\Elemental\Models\ElementalArea;
 use NumberFormatter;
 use SilverStripe\Config\MergeStrategy\Priority;
+use SilverStripe\Forms\FormField;
 use SilverStripe\Forms\Validator;
 use SilverStripe\i18n\i18n;
 use SilverStripe\ORM\ArrayList;
@@ -50,7 +51,7 @@ if (class_exists(ElementalAreaField::class) && class_exists(ElementalArea::class
             $elementClassesToCheck = $this->required;
             $errors = [];
 
-            // Validate against all elemental areas, unless configuration indicates a specific area.
+            // Get the elemental areas to be validated against.
             foreach ($this->form->Fields()->dataFields() as $fieldName => $field) {
                 if (!$field instanceof ElementalAreaField) {
                     continue;
@@ -58,86 +59,115 @@ if (class_exists(ElementalAreaField::class) && class_exists(ElementalArea::class
                 $elementalAreaFields->add($field);
             }
 
+            // Validate elemental areas.
             foreach ($elementalAreaFields as $field) {
-                $fieldName = $field->Name;
-                /** @var ElementalArea $area */
-                $area = $field->getArea();
-                if ($area) {
-                    // Get block positioning
-                    $blockPositions = $this->getBlockPositions($area);
-
-                    // Check submitted form data for each element in the area.
-                    foreach ($area->Elements() as $element) {
-                        if (!array_key_exists($element->ClassName, $elementClassesToCheck)) {
-                            continue;
-                        }
-                        $requiredConfig = $elementClassesToCheck[$element->ClassName];
-                        // If we need to check for this block in specific fields but not this one, skip for now.
-                        if (
-                            !empty($requiredConfig['areafieldname'])
-                            && !in_array($fieldName, $requiredConfig['areafieldname'])
-                        ) {
-                            continue;
-                        }
-                        // Make sure an errors array exists
-                        if (!isset($errors[$field->Name][$element->ClassName])) {
-                            $errors[$field->Name][$element->ClassName] = [];
-                        }
-                        $relevantFields = $this->getRelevantFields($elementalAreaFields, $requiredConfig);
-
-                        // Validate against minimum and maximum number of blocks.
-                        $numberOfBlocks = $this->getNumberOfBlocks($element->ClassName, $relevantFields);
-                        $this->validateMinMax(
-                            $requiredConfig,
-                            $numberOfBlocks,
-                            $relevantFields,
-                            $errors[$field->Name][$element->ClassName]
-                        );
-
-                        // Validate against position.
-                        $this->validatePosition(
-                            $requiredConfig,
-                            $blockPositions[$element->ClassName],
-                            $errors[$field->Name][$element->ClassName]
-                        );
-
-                        // Remove errors array if empty.
-                        if (empty($errors[$field->Name][$element->ClassName])) {
-                            unset($errors[$field->Name][$element->ClassName]);
-                        }
-
-                        // Don't check this block class again.
-                        unset($elementClassesToCheck[$element->ClassName]);
-                    }
-                }
-
+                $this->validateElementalArea($field, $elementalAreaFields, $elementClassesToCheck, $errors);
                 // Remove errors array if empty.
                 if (empty($errors[$field->Name])) {
                     unset($errors[$field->Name]);
                 }
             }
 
-            // Add an error for each missing elemental block type if the config for the class states a minimum.
-            if (!empty($elementClassesToCheck)) {
-                foreach ($elementClassesToCheck as $className => $requiredConfig) {
-                    if (isset($requiredConfig['min'])) {
-                        $relevantFields = $this->getRelevantFields($elementalAreaFields, $requiredConfig);
-                        foreach ($relevantFields as $field) {
-                            $errors[$field->Name][$className][] = self::TOO_FEW_ERROR;
-                            if (isset($requiredConfig['pos'])) {
-                                $errors[$field->Name][$className][] = self::POSITION_ERROR;
-                            }
-                        }
-                    }
-                }
-            }
+            // Validate any missing required blocks.
+            $this->validateMissingBlocks($elementalAreaFields, $elementClassesToCheck, $errors);
 
             // Add error messages to fields.
             if (!empty($errors)) {
                 $this->setErrorMessages($errors);
             }
-
             return empty($errors);
+        }
+
+        /**
+         * Add an error for each missing elemental block type if the config for the class states a minimum.
+         *
+         * @param ArrayList $elementalAreaFields
+         * @param array $elementClassesToCheck
+         * @param array $errors
+         */
+        protected function validateMissingBlocks(
+            ArrayList $elementalAreaFields,
+            array $elementClassesToCheck,
+            array &$errors
+        ) {
+            foreach ($elementClassesToCheck as $className => $requiredConfig) {
+                if (isset($requiredConfig['min'])) {
+                    $relevantFields = $this->getRelevantFields($elementalAreaFields, $requiredConfig);
+                    foreach ($relevantFields as $field) {
+                        $errors[$field->Name][$className][] = self::TOO_FEW_ERROR;
+                        if (isset($requiredConfig['pos'])) {
+                            $errors[$field->Name][$className][] = self::POSITION_ERROR;
+                        }
+                    }
+                }
+            }
+        }
+
+        /**
+         * Validate a specific ElementalArea.
+         *
+         * @param FormField $field
+         * @param ArrayList $elementalAreaFields
+         * @param array $elementClassesToCheck
+         * @param array $errors
+         */
+        protected function validateElementalArea(
+            FormField $field,
+            ArrayList $elementalAreaFields,
+            array &$elementClassesToCheck,
+            array &$errors
+        ) {
+            $fieldName = $field->Name;
+            /** @var ElementalArea $area */
+            $area = $field->getArea();
+            if ($area) {
+                // Get block positioning
+                $blockPositions = $this->getBlockPositions($area);
+
+                // Check submitted form data for each element in the area.
+                foreach ($area->Elements() as $element) {
+                    if (!array_key_exists($element->ClassName, $elementClassesToCheck)) {
+                        continue;
+                    }
+                    $requiredConfig = $elementClassesToCheck[$element->ClassName];
+                    // If we need to check for this block in specific fields but not this one, skip for now.
+                    if (
+                        !empty($requiredConfig['areafieldname'])
+                        && !in_array($fieldName, $requiredConfig['areafieldname'])
+                    ) {
+                        continue;
+                    }
+                    // Make sure an errors array exists
+                    if (!isset($errors[$field->Name][$element->ClassName])) {
+                        $errors[$field->Name][$element->ClassName] = [];
+                    }
+                    $relevantFields = $this->getRelevantFields($elementalAreaFields, $requiredConfig);
+
+                    // Validate against minimum and maximum number of blocks.
+                    $numberOfBlocks = $this->getNumberOfBlocks($element->ClassName, $relevantFields);
+                    $this->validateMinMax(
+                        $requiredConfig,
+                        $numberOfBlocks,
+                        $relevantFields,
+                        $errors[$field->Name][$element->ClassName]
+                    );
+
+                    // Validate against position.
+                    $this->validatePosition(
+                        $requiredConfig,
+                        $blockPositions[$element->ClassName],
+                        $errors[$field->Name][$element->ClassName]
+                    );
+
+                    // Remove errors array if empty.
+                    if (empty($errors[$field->Name][$element->ClassName])) {
+                        unset($errors[$field->Name][$element->ClassName]);
+                    }
+
+                    // Don't check this block class again.
+                    unset($elementClassesToCheck[$element->ClassName]);
+                }
+            }
         }
 
         /**
