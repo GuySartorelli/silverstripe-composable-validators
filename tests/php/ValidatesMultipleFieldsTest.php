1441e0e1d8b41aafaabba3d893afc04a5f0c11c4
Guy Sartorelli <guy.sartorelli@signify.co.nz>
2021-08-17 16:14:37 +1200

    feat: Add tests for the traits.

--- tests/php/ValidatesMultipleFieldsTest.php
+++ tests/php/ValidatesMultipleFieldsTest.php
@@ -6,4 +6,158 @@ use SilverStripe\Dev\SapphireTest;
 
 class ValidatesMultipleFieldsTest extends SapphireTest
 {
+    private function getFieldsForTests()
+    {
+        return [
+            'Title',
+            'Content',
+            'Image',
+            'AnotherField',
+        ];
+    }
+
+    /**
+     * Should be able to instantiate with no arguments.
+     */
+    public function testConstructingWithoutFields()
+    {
+        $validator = new TestMultiFieldValidator();
+        $this->assertEmpty($validator->getFields());
+    }
+
+    /**
+     * Should be able to instantiate with an array of field names.
+     */
+    public function testConstructingWithArray()
+    {
+        $fields = $this->getFieldsForTests();
+        $validator = new TestMultiFieldValidator($fields);
+        $this->assertEquals($fields, $validator->getFields());
+    }
+
+    /**
+     * Should be able to instantiate with an argument list of field names.
+     */
+    public function testConstructingWithArguments()
+    {
+        $validator = new TestMultiFieldValidator(
+            'Title',
+            'Content',
+            'Image',
+            'AnotherField'
+        );
+        $this->assertEquals($this->getFieldsForTests(), $validator->getFields());
+    }
+
+    /**
+     * Should be able to remove all fields at once.
+     */
+    public function testRemoveValidation()
+    {
+        $validator = new TestMultiFieldValidator($this->getFieldsForTests());
+        $validator->removeValidation();
+        $this->assertEmpty($validator->getFields());
+    }
+
+    /**
+     * Should be able to remove fields one at a time, by name.
+     */
+    public function testRemoveField()
+    {
+        $fields = $this->getFieldsForTests();
+        $validator = new TestMultiFieldValidator($fields);
+        // Fields should be easy to remove from the list.
+        $removeField = array_shift($fields);
+        $validator->removeField($removeField);
+        $this->assertEquals($fields, $validator->getFields());
+        // Check that it wasn't just removing the first item.
+        $removeField = array_pop($fields);
+        $validator->removeField($removeField);
+        $this->assertEquals($fields, $validator->getFields());
+        // Attempting to remove a field that isn't in the list should not mutate the list.
+        $validator->removeField('MissingField');
+        $this->assertEquals($fields, $validator->getFields());
+    }
+
+    /**
+     * Should be able to remove many fields at once by name.
+     */
+    public function testRemoveFields()
+    {
+        $fields = $this->getFieldsForTests();
+        $validator = new TestMultiFieldValidator($fields);
+        // Fields should be easy to remove from the list.
+        $removeFields = [array_shift($fields), array_pop($fields)];
+        $validator->removeFields($removeFields);
+        $this->assertEquals($fields, $validator->getFields());
+        // Attempting to remove fields that aren't in the list should not mutate the list.
+        $validator->removeFields(['MissingField']);
+        $this->assertEquals($fields, $validator->getFields());
+    }
+
+    /**
+     * Should be able to add fields one at a time, by name.
+     */
+    public function testAddField()
+    {
+        $validator = new TestMultiFieldValidator();
+        // Add a couple of fields.
+        $validator->addField('Title');
+        $this->assertEquals(['Title'], $validator->getFields());
+        $validator->addField('Content');
+        $this->assertEquals(
+            [
+                'Title',
+                'Content',
+            ],
+            $validator->getFields()
+        );
+        // Make sure that the same field can't be added twice.
+        $validator->addField('Content');
+        $this->assertEquals(
+            [
+                'Title',
+                'Content',
+            ],
+            $validator->getFields()
+        );
+    }
+
+    /**
+     * Should be able to add many fields at once by name.
+     */
+    public function testAddFields()
+    {
+        $validator = new TestMultiFieldValidator();
+        // Add a couple of fields.
+        $validator->addFields(['Title']);
+        $this->assertEquals(['Title'], $validator->getFields());
+        $validator->addFields([
+            'Content',
+            'Image',
+        ]);
+        $this->assertEquals(
+            [
+                'Title',
+                'Content',
+                'Image',
+            ],
+            $validator->getFields()
+        );
+        // Make sure that the same fields can't be added twice.
+        $validator->addFields([
+            'Content',
+            'Image',
+            'AnotherField',
+        ]);
+        $this->assertEquals(
+            [
+                'Title',
+                'Content',
+                'Image',
+                'AnotherField',
+            ],
+            $validator->getFields()
+        );
+    }
 }
--- tests/php/ValidatesMultipleFieldsWithConfigTest.php
+++ tests/php/ValidatesMultipleFieldsWithConfigTest.php
@@ -6,4 +6,161 @@ use SilverStripe\Dev\SapphireTest;
 
 class ValidatesMultipleFieldsWithConfigTest extends SapphireTest
 {
+    private function getFieldsForTests()
+    {
+        return [
+            'Title' => ['config1'],
+            'Content' => ['config2'],
+            'Image' => ['config3'],
+            'AnotherField' => ['config4'],
+        ];
+    }
+
+    /**
+     * Should be able to instantiate with no arguments or an empty array.
+     */
+    public function testConstructingWithoutFields()
+    {
+        $validator = new TestMultiFieldWithConfigValidator();
+        $this->assertEmpty($validator->getFields());
+        $validator = new TestMultiFieldWithConfigValidator([]);
+        $this->assertEmpty($validator->getFields());
+    }
+
+    /**
+     * Should be able to instantiate with an array of field names and configuration arrays.
+     */
+    public function testConstructingWithArray()
+    {
+        $fields = $this->getFieldsForTests();
+        $validator = new TestMultiFieldWithConfigValidator($fields);
+        $this->assertEquals($fields, $validator->getFields());
+    }
+
+    /**
+     * Should not be able to instantiate with an array of field names and empty configuration arrays.
+     * @expectedException \InvalidArgumentException
+     */
+    public function testConstructingWithEmptyArray()
+    {
+        new TestMultiFieldWithConfigValidator(['someField' => []]);
+    }
+
+    /**
+     * Should be able to remove all fields at once.
+     */
+    public function testRemoveValidation()
+    {
+        $validator = new TestMultiFieldWithConfigValidator($this->getFieldsForTests());
+        $validator->removeValidation();
+        $this->assertEmpty($validator->getFields());
+    }
+
+    /**
+     * Should be able to remove fields one at a time, by name.
+     */
+    public function testRemoveField()
+    {
+        $fields = $this->getFieldsForTests();
+        $fieldNames = array_keys($fields);
+        $validator = new TestMultiFieldWithConfigValidator($fields);
+        // Fields should be easy to remove from the list.
+        $removeField = array_shift($fieldNames);
+        unset($fields[$removeField]);
+        $validator->removeField($removeField);
+        $this->assertEquals($fields, $validator->getFields());
+        // Check that it wasn't just removing the first item.
+        $removeField = array_pop($fieldNames);
+        unset($fields[$removeField]);
+        $validator->removeField($removeField);
+        $this->assertEquals($fields, $validator->getFields());
+        // Attempting to remove a field that isn't in the list should not mutate the list.
+        $validator->removeField('MissingField');
+        $this->assertEquals($fields, $validator->getFields());
+    }
+
+    /**
+     * Should be able to remove many fields at once by name.
+     */
+    public function testRemoveFields()
+    {
+        $fields = $this->getFieldsForTests();
+        $fieldNames = array_keys($fields);
+        $validator = new TestMultiFieldWithConfigValidator($fields);
+        // Fields should be easy to remove from the list.
+        $removeFields = [array_shift($fieldNames), array_pop($fieldNames)];
+        array_shift($fields);
+        array_pop($fields);
+        $validator->removeFields($removeFields);
+        $this->assertEquals($fields, $validator->getFields());
+        // Attempting to remove fields that aren't in the list should not mutate the list.
+        $validator->removeFields(['MissingField']);
+        $this->assertEquals($fields, $validator->getFields());
+    }
+
+    /**
+     * Should be able to add fields one at a time, by name.
+     */
+    public function testAddField()
+    {
+        $validator = new TestMultiFieldWithConfigValidator();
+        // Add a couple of fields.
+        $validator->addField('Title', ['config1']);
+        $this->assertEquals(['Title' => ['config1']], $validator->getFields());
+        $validator->addField('Content', ['config2']);
+        $this->assertEquals(
+            [
+                'Title' => ['config1'],
+                'Content' => ['config2'],
+            ],
+            $validator->getFields()
+        );
+        // Make sure that the same field can't be added twice, but config does get updated.
+        $validator->addField('Content', ['confignew']);
+        $this->assertEquals(
+            [
+                'Title' => ['config1'],
+                'Content' => ['confignew'],
+            ],
+            $validator->getFields()
+        );
+    }
+
+    /**
+     * Should be able to add many fields at once by name.
+     */
+    public function testAddFields()
+    {
+        $validator = new TestMultiFieldWithConfigValidator();
+        // Add a couple of fields.
+        $validator->addFields(['Title' => ['config1']]);
+        $this->assertEquals(['Title' => ['config1']], $validator->getFields());
+        $validator->addFields([
+            'Content' => ['config2'],
+            'Image' => ['config3'],
+        ]);
+        $this->assertEquals(
+            [
+                'Title' => ['config1'],
+                'Content' => ['config2'],
+                'Image' => ['config3'],
+            ],
+            $validator->getFields()
+        );
+        // Make sure that the same fields can't be added twice, but config is updated.
+        $validator->addFields([
+            'Content' => ['confignew'],
+            'Image' => ['confignew2'],
+            'AnotherField' => ['config4'],
+        ]);
+        $this->assertEquals(
+            [
+                'Title' => ['config1'],
+                'Content' => ['confignew'],
+                'Image' => ['confignew2'],
+                'AnotherField' => ['config4'],
+            ],
+            $validator->getFields()
+        );
+    }
 }
