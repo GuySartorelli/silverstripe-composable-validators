f91705a99ab88181807fa0f87019aff500336113
Guy Sartorelli <guy.sartorelli@signify.co.nz>
2021-08-17 14:18:41 +1200

    feat: Make it easier to test specific validators and tab config.

--- tests/php/TestOnlyClasses/TestFormGenerator.php
+++ tests/php/TestOnlyClasses/TestFormGenerator.php
@@ -5,7 +5,9 @@ namespace Signify\ComposableValidators\Tests;
 use SilverStripe\Dev\TestOnly;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\Form;
+use SilverStripe\Forms\TabSet;
 use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\Validator;
 
 class TestFormGenerator implements TestOnly
 {
@@ -13,19 +15,31 @@ class TestFormGenerator implements TestOnly
      * Common method for setting up a test form.
      *
      * @param string[] $fieldNames
+     * @param Validator|null $validator
+     * @param string|null $tab
      * @return Form
      */
-    public static function getForm(array $fieldNames = [])
+    public static function getForm(array $fieldNames = [], ?Validator $validator = null, $tab = null)
     {
         $fieldList = new FieldList();
+        if ($tab) {
+            $root = explode('.', $tab)[0];
+            $fieldList->add(TabSet::create($root));
+            $fieldList->findOrMakeTab($tab);
+        }
         foreach ($fieldNames as $name => $value) {
             if (is_numeric($name)) {
                 $name = $value;
                 $value = null;
             }
-            $fieldList->add(TextField::create($name)->setValue($value));
+            $field = TextField::create($name)->setValue($value);
+            if ($tab) {
+                $fieldList->addFieldToTab($tab, $field);
+            } else {
+                $fieldList->add($field);
+            }
         }
 
-        return new Form(null, 'testForm', $fieldList, new FieldList([/* no actions */]));
+        return new Form(null, 'testForm', $fieldList, new FieldList([/* no actions */]), $validator);
     }
 }
